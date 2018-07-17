9c67ee91b32fd6d2764dc69f973197e59f66955b
Andrew Nguyen Vo <awnguy01@louisville.edu>
2018-07-17 11:56:14 -0400

    Final Project Rebased

--- Final Project/Final Project/src/main.c
+++ Final Project/Final Project/src/main.c
@@ -1,20 +1,25 @@
 
- // Lab3P1.c
- //
- // Created: 1/30/2018 4:04:52 AM
- // Author : Eugene Rockey
- // Copyright 2018, All Rights Reserved
- 
- //no includes, no ASF, no libraries
- 
- const char MS1[] = "\r\nECE-412 ATMega328P Tiny OS";
- const char MS2[] = "\r\nby Eugene Rockey Copyright 2018, All Rights Reserved";
- const char MS3[] = "\r\nMenu: (L)CD, (A)CD, (E)EPROM\r\n";
- const char MS4[] = "\r\nReady: ";
- const char MS5[] = "\r\nInvalid Command Try Again...";
- const char MS6[] = "Volts\r";
- 
- 
+// Lab3P1.c
+//
+// Created: 1/30/2018 4:04:52 AM
+// Author : Eugene Rockey
+// Copyright 2018, All Rights Reserved
+
+#define F_CPU 16000000
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <util/delay.h>
+
+const char MS1[] = "\r\nECE-412 ATMega328P Tiny OS";
+const char MS2[] = "\r\nby Eugene Rockey Copyright 2018, All Rights Reserved";
+const char MS3[] = "\r\nMenu: (L)CD, (A)DC, (E)EPROM, (U)SART\r\n";
+const char MS4[] = "\r\nReady: ";
+const char MS5[] = "\r\nInvalid Command Try Again...";
+const char MS6[] = "Volts\r";
+const char MS7[] = " F";
+const char teamName[] = "               Four is Better Than Five";
 
 void LCD_Init(void);			//external Assembly functions
 void UART_Init(void);
@@ -28,77 +33,148 @@ void Mega328P_Init(void);
 
 unsigned char ASCII;			//shared I/O variable with Assembly
 unsigned char DATA;				//shared internal variable with Assembly
-char HADC;						//shared ADC variable with Assembly
-char LADC;						//shared ADC variable with Assembly
+unsigned char hAddress;			//high byte of address used in EEPROM
+unsigned char lAddress;			//low byte of address used in EEPROM
+unsigned char content;			//content to write in EEPROM
+unsigned int count;				//counter variable
 
-char volts[5];					//string buffer for ADC output
-int Acc;						//Accumulator for ADC use
-
-void UART_Puts(const char *str)	//Display a string in the PC Terminal Program
+/**
+*UART_Puts
+*Display a string in the PC Terminal Program
+*/
+void UART_Puts(const char *str)
 {
 	while (*str)
 	{
 		ASCII = *str++;
 		UART_Put();
 	}
-}
+} // end UART_Puts
 
-void LCD_Puts(const char *str)	//Display a string on the LCD Module
+/**
+*LCD_Puts
+*Display a string on the LCD Module
+*/
+void LCD_Puts(const char *str)
 {
 	while (*str)
 	{
 		DATA = *str++;
 		LCD_Write_Data();
 	}
-}
+} // end LCD_Puts
 
-
-void Banner(void)				//Display Tiny OS Banner on Terminal
+/**
+*Banner
+*Display Tiny OS Banner on Terminal
+*/
+void Banner(void)
 {
 	UART_Puts(MS1);
 	UART_Puts(MS2);
 	UART_Puts(MS4);
-}
+} // end Banner
 
-void LCD(void)						//Lite LCD demo
-{
-	DATA = 0x34;					//Student Comment Here
-	LCD_Write_Command();
-	DATA = 0x08;					//Student Comment Here
-	LCD_Write_Command();
-	DATA = 0x02;					//Student Comment Here
-	LCD_Write_Command();
-	DATA = 0x06;					//Student Comment Here
-	LCD_Write_Command();
-	DATA = 0x0f;					//Student Comment Here
-	LCD_Write_Command();
-	LCD_Puts("Hello ECE412!");
-	/*
-	Re-engineer this subroutine to have the LCD endlessly scroll a marquee sign of 
-	your Team's name either vertically or horizontally. Any key press should stop
-	the scrolling and return execution to the command line in Terminal. User must
-	always be able to return to command line.
-	*/
-}
-
-void Command(void)					//command interpreter
+/**
+*HELP
+*Display available Tiny OS Commands on Terminal
+*/
+void HELP(void)
 {
 	UART_Puts(MS3);
-	ASCII = '\0';						
+} // end HELP
+
+/**
+*LCD
+*/
+void LCD(void)						//Lite LCD demo
+{
+	DATA = 0x34;					//Sets data bits in LCD
+	LCD_Write_Command();
+	DATA = 0x08;					//Clearing display without clearing DDRAM content
+	LCD_Write_Command();
+	DATA = 0x02;					//Set cursor position to 0
+	LCD_Write_Command();
+	DATA = 0x06;					//Entry Mode
+	LCD_Write_Command();
+	DATA = 0x0f;					//Display on cursor blinking
+	LCD_Write_Command();
+	LCD_Puts(teamName);
+	ASCII = '\0';
+	while (ASCII == '\0') {
+		for (count = 0; count < 40; count ++) {
+			DATA = 0x18;
+			LCD_Write_Command();
+			_delay_ms(200);
+			
+			asm("lds r16,0xC6"); // check what ASCII value is being stored
+			asm("sts ASCII,r16");
+
+			if (ASCII != '\0') {
+				DATA = 0x01;
+				LCD_Write_Command();
+				return;
+			}
+		}
+		DATA = 0x02;
+		LCD_Write_Command();
+	}
+	return;
+} // end LCD
+
+
+/**
+*USART
+*Prompts user to change the Baud rate, character size, parity, stop bits, or exit back to menu
+*/
+void USART(void) {
+	int dataBits = 0;
+	int parity = 0;
+	int stopBits = 0;
+	UART_Puts("\r\nSelect what you would like to change\r\n");
+	UART_Puts("\r\n(1)Baud Rate (2)Character Size (3)Parity (4)Stop bits (ESC) Menu\r\n");
+	UART_Get();
+	switch(ASCII) {
+		case 27:
+		return;
+		default: UART_Puts("Invalid Option");
+		break;
+	}
+	return;
+} // end USART
+
+/**
+* command interpreter
+*/
+void Command(void)
+{
+	UART_Puts(MS3);
+	ASCII = '\0';
 	while (ASCII == '\0')
 	{
 		UART_Get();
 	}
 	switch (ASCII)
 	{
-		case 'L' | 'l': LCD();
+		case 'l':
+		case 'L':
+		LCD();
+		break;
+		case 'u':
+		case 'U':
+		USART();
 		break;
 		default:
 		UART_Puts(MS5);
-		break;  			//Add a 'USART' command and subroutine to allow the user to reconfigure the 						//serial port parameters during runtime. Modify baud rate, # of data bits, parity, 							//# of stop bits.
+		break;  			//Add a 'USART' command and subroutine to allow the user to reconfigure the
+		//serial port parameters during runtime. Modify baud rate, # of data bits, parity,
+		//# of stop bits.
 	}
-}
+} // end Command
 
+/**
+* main
+*/
 int main(void)
 {
 	Mega328P_Init();
@@ -107,5 +183,5 @@ int main(void)
 	{
 		Command();				//infinite command loop
 	}
-}
+} // end Main
 
