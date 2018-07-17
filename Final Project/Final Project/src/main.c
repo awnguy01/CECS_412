4328e00e26281698efca875e4c15d6b3f8feac68
Andrew Nguyen Vo <awnguy01@louisville.edu>
2018-07-17 11:31:50 -0400

    Final Project Base

---
+++ Final Project/Final Project/src/main.c
@@ -0,0 +1,111 @@
+
+ // Lab3P1.c
+ //
+ // Created: 1/30/2018 4:04:52 AM
+ // Author : Eugene Rockey
+ // Copyright 2018, All Rights Reserved
+ 
+ //no includes, no ASF, no libraries
+ 
+ const char MS1[] = "\r\nECE-412 ATMega328P Tiny OS";
+ const char MS2[] = "\r\nby Eugene Rockey Copyright 2018, All Rights Reserved";
+ const char MS3[] = "\r\nMenu: (L)CD, (A)CD, (E)EPROM\r\n";
+ const char MS4[] = "\r\nReady: ";
+ const char MS5[] = "\r\nInvalid Command Try Again...";
+ const char MS6[] = "Volts\r";
+ 
+ 
+
+void LCD_Init(void);			//external Assembly functions
+void UART_Init(void);
+void UART_Clear(void);
+void UART_Get(void);
+void UART_Put(void);
+void LCD_Write_Data(void);
+void LCD_Write_Command(void);
+void LCD_Read_Data(void);
+void Mega328P_Init(void);
+
+unsigned char ASCII;			//shared I/O variable with Assembly
+unsigned char DATA;				//shared internal variable with Assembly
+char HADC;						//shared ADC variable with Assembly
+char LADC;						//shared ADC variable with Assembly
+
+char volts[5];					//string buffer for ADC output
+int Acc;						//Accumulator for ADC use
+
+void UART_Puts(const char *str)	//Display a string in the PC Terminal Program
+{
+	while (*str)
+	{
+		ASCII = *str++;
+		UART_Put();
+	}
+}
+
+void LCD_Puts(const char *str)	//Display a string on the LCD Module
+{
+	while (*str)
+	{
+		DATA = *str++;
+		LCD_Write_Data();
+	}
+}
+
+
+void Banner(void)				//Display Tiny OS Banner on Terminal
+{
+	UART_Puts(MS1);
+	UART_Puts(MS2);
+	UART_Puts(MS4);
+}
+
+void LCD(void)						//Lite LCD demo
+{
+	DATA = 0x34;					//Student Comment Here
+	LCD_Write_Command();
+	DATA = 0x08;					//Student Comment Here
+	LCD_Write_Command();
+	DATA = 0x02;					//Student Comment Here
+	LCD_Write_Command();
+	DATA = 0x06;					//Student Comment Here
+	LCD_Write_Command();
+	DATA = 0x0f;					//Student Comment Here
+	LCD_Write_Command();
+	LCD_Puts("Hello ECE412!");
+	/*
+	Re-engineer this subroutine to have the LCD endlessly scroll a marquee sign of 
+	your Team's name either vertically or horizontally. Any key press should stop
+	the scrolling and return execution to the command line in Terminal. User must
+	always be able to return to command line.
+	*/
+}
+
+void Command(void)					//command interpreter
+{
+	UART_Puts(MS3);
+	ASCII = '\0';						
+	while (ASCII == '\0')
+	{
+		UART_Get();
+	}
+	switch (ASCII)
+	{
+		case 'L' | 'l': LCD();
+		break;
+		default:
+		UART_Puts(MS5);
+		break;  			//Add a 'USART' command and subroutine to allow the user to reconfigure the 						//serial port parameters during runtime. Modify baud rate, # of data bits, parity, 							//# of stop bits.
+	}
+}
+
+int main(void)
+{
+	Mega328P_Init();
+	Banner();
+	while (1)
+	{
+		Command();				//infinite command loop
+	}
+}
+
